d2d326f1f9936ab0a76304b47fbfd92bbfd8f605
swatichauhan09 <86587715+swatichauhan09@users.noreply.github.com>
2021-06-28 11:57:29 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,21 @@
+import operator
+text = open("logfile.txt", "r")
+d = dict()
+ips = []
+i=0
+for line in text:
+    line = line.strip()
+    words= line.split("- -")
+    ips=words[0]
+    ips=ips.split()
+    for word in ips:
+        if word in d:
+            d[word] = d[word] + 1
+        else:
+            d[word] = 1
+sorted_d = dict( sorted(d.items(), key=operator.itemgetter(1),reverse=True))
+for key in list(sorted_d.keys()):
+    if i<8:
+        print(key," ",sorted_d[key])
+        i=i+1
+        
